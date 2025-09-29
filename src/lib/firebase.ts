f1b7cdd5fe3954bcc4470549f93a2ecdce17bd46
Матвей Стасевич <yngmatteo58@gmail.com>
2025-09-29 17:16:17 +0000

    Try fixing this error: `Console Error: Error: Firebase configuration err

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBWI_p1rd_9WZSpnC8YSIXCmA193Jb_xVs",
-  authDomain: "studio-5422558187-9cb0a.firebaseapp.com",
-  projectId: "studio-5422558187-9cb0a",
-  storageBucket: "studio-5422558187-9cb0a.firebasestorage.app",
-  messagingSenderId: "32584903617",
-  appId: "1:32584903617:web:169d11286b7031a540dd40"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase for SSR
