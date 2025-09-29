5bd7459dca3c05b46022b322dffa7b780fac888e
Матвей Стасевич <yngmatteo58@gmail.com>
2025-09-29 16:06:36 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -62,7 +62,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     logout,
   };
 
-  return <AuthContext.Provider value={value}>{!loading && children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
 export const useAuth = () => {
