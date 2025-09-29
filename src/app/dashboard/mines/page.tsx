50c4ead48a47b7111a04ff504afac93bf9c1e93b
Матвей Стасевич <yngmatteo58@gmail.com>
2025-09-29 09:57:17 +0000

    ну в пределах разумного меньше как и все режимы

--- src/app/dashboard/mines/page.tsx
+++ src/app/dashboard/mines/page.tsx
@@ -2,8 +2,8 @@ import MinesGame from '@/components/games/mines-game';
 
 export default function MinesPage() {
   return (
-    <main className="flex flex-1 flex-col items-center justify-start p-4 sm:p-6">
-      <div className="w-full">
+    <main className="flex flex-1 flex-col items-center justify-center p-4 sm:p-6">
+      <div className="w-full max-w-4xl">
         <MinesGame />
       </div>
     </main>
